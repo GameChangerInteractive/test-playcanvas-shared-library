e18dcefa72c767be1ba6ac95cc4ede8c16faaf26
Michael Cooper <MTCooper115@gmail.com>
2021-06-30 10:34:03 -0400

    Initial Commit

---
+++ prepare.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+
+buildPackage();
+
+async function buildPackage() {
+    const content = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+    const buildNum = process.env.CIRCLE_BUILD_NUM;
+
+    if (buildNum != null) {
+        const version = content.version.split('.');
+        version[2] = buildNum;
+        content.version = version.join('.');
+    }
+
+    delete content.devDependencies;
+    delete content.scripts;
+    delete content.husky;
+    content.main = 'index.js';
+    content.types = 'index.d.ts';
+    fs.writeFileSync(path.join('dist', 'package.json'), JSON.stringify(content, null, 2), 'utf8');
+    shell.cp('.npmrc', path.join('dist', '.npmrc'));
+    shell.mkdir('-p', path.join('dist', 'misc'));
+    shell.cp(path.join('src', 'misc', 'pubnub.js'), path.join('dist', 'misc', 'pubnub.js'));
+}
